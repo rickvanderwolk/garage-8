52561353c22ee985d9419e2e5cd20c995a590756
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-03-14 17:45:03 +0100

    Add rps

---
+++ rps/game.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+choices=("r" "p" "s")
+player_score=0
+computer_score=0
+ties=0
+total_games=0
+
+show_choice() {
+    case $1 in
+        r) echo "Rock 🪨" ;;
+        p) echo "Paper 📄" ;;
+        s) echo "Scissors ✂️" ;;
+    esac
+}
+
+determine_winner() {
+    ((total_games++))
+    if [[ "$1" == "$2" ]]; then
+        echo "🟡 It's a tie"
+        ((ties++))
+    elif [[ ("$1" == "r" && "$2" == "s") || ("$1" == "p" && "$2" == "r") || ("$1" == "s" && "$2" == "p") ]]; then
+        echo "🟢 You win"
+        ((player_score++))
+    else
+        echo "🔴 Computer wins"
+        ((computer_score++))
+    fi
+}
+
+display_score() {
+    win_percentage=0
+    loss_percentage=0
+    tie_percentage=0
+
+    if ((total_games > 0)); then
+        win_percentage=$((player_score * 100 / total_games))
+        loss_percentage=$((computer_score * 100 / total_games))
+        tie_percentage=$((ties * 100 / total_games))
+    fi
+
+    echo -e "\n📊 Score: Player 🟢 $player_score ($win_percentage%) | Computer 🔴 $computer_score ($loss_percentage%) | Ties 🟡 $ties ($tie_percentage%)"
+
+    if ((player_score > computer_score)); then
+        echo "🏆 Status: 🟢 You are leading"
+    elif ((computer_score > player_score)); then
+        echo "🏆 Status: 🔴 Computer is leading"
+    else
+        echo "🏆 Status: 🟡 It's a tie"
+    fi
+    echo
+}
+
+display_instructions() {
+    echo -e "\n-----------------------------------------------"
+    echo "Press: 'r' for Rock 🪨 | 'p' for Paper 📄 | 's' for Scissors ✂️"
+    echo "Press Ctrl+C to quit the game."
+    echo "-----------------------------------------------"
+}
+
+echo "==============================================="
+echo "🪨📄✂️  Rock-Paper-Scissors"
+echo "==============================================="
+
+while true; do
+    display_instructions
+
+    read -n1 -s player_choice
+
+    if [[ "$player_choice" =~ ^[rps]$ ]]; then
+        computer_choice=${choices[$RANDOM % 3]}
+
+        echo -e "\n-----------------------------------------------"
+        echo -n "🧑 You chose:      "; show_choice "$player_choice"
+        echo -n "💻 Computer chose: "; show_choice "$computer_choice"
+
+        determine_winner "$player_choice" "$computer_choice"
+        display_score
+    fi
+done
