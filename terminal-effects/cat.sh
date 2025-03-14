f06d55015ff3ef9b6c6e1405bf6649585b3c6eaa
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-03-15 00:46:20 +0100

    Add cat

---
+++ terminal-effects/cat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
+trap "tput cnorm; exit" INT
+
+tput civis
+
+clear
+
+frame1="
+    |\_/|
+    |0 0|
+   =\_Y_/=
+    /   \\
+"
+
+frame2="
+    |\_/|
+    |0 -|
+   =\_Y_/=
+    /   \\
+"
+
+frames=("$frame1" "$frame1" "$frame1" "$frame1" "$frame1" "$frame2" "$frame1")
+
+while true; do
+    for frame in "${frames[@]}"; do
+        clear
+        echo -e "$frame"
+        sleep 0.3
+    done
+done
+
+tput cnorm
