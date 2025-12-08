927affb4d23d8657ceed259bb4c2e38a78acd1ed
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-12-08 12:28:02 +0100

    Add sketch selector + URL query param

---
+++ p5/sketch-loader.js
@@ -0,0 +1,22 @@
+(function() {
+  const params = new URLSearchParams(window.location.search);
+  const sketchId = params.get('sketch') || '1';
+
+  const select = document.getElementById('sketch-select');
+
+  for (let i = 1; i <= 21; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = `Sketch ${i}`;
+    if (i == sketchId) option.selected = true;
+    select.appendChild(option);
+  }
+
+  select.onchange = function() {
+    window.location.search = '?sketch=' + select.value;
+  };
+
+  const script = document.createElement('script');
+  script.src = 'sketches/' + sketchId + '.js';
+  document.head.appendChild(script);
+})();
